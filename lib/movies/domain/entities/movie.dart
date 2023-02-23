2dbbfa2891d6a436e0d7b9ef54327c31982b6088
Afwra <ahmedgamal5179@gmail.com>
2023-02-23 14:47:13 +0200

    Initial commit

---
+++ lib/movies/domain/entities/movie.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable{
+  final int id;
+  final String title;
+  final String backdropPath;
+  final List<int> genderIds;
+  final String overview;
+  final double voteAvg;
+
+  const Movie({
+    required this.id,
+    required this.title,
+    required this.backdropPath,
+    required this.genderIds,
+    required this.overview,
+    required this.voteAvg,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [id,title,backdropPath,genderIds,overview,voteAvg];
+
+
+
+
+}
