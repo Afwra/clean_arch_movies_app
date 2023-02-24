8df42b74823fa7bb989f891549e0dcc19af2a836
Ahmed Gamal Abd El Fatah <101475366+Afwra@users.noreply.github.com>
2023-02-24 15:18:31 +0200

    Update api_constants.dart

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants{
   static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String apiKey = '1232c9666905fa56df2a04ec8cf24e6b';
+  static const String apiKey = '';
 
   static const String nowPlayingPath = '$baseUrl/movie/now_playing?api_key=$apiKey';
   static const String popularMoviesPath = '$baseUrl/movie/popular?api_key=$apiKey';
